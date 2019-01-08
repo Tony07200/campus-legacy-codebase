5a6e05f33d9c7a292208d7ce348d8596e106706a
Tony07200 <baptiste.anthony07@gmail.com>
2019-01-08 16:34:04 +0100

    fix

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -56,7 +56,7 @@ public class Item {
         this.increaseQuality();
         if(this.sellIn<= 10) this.increaseQuality();
         if(this.sellIn<= 5) this.increaseQuality();
-        if(this.sellIn <= 0) this.quality = 0;
+        if(this.sellIn < 0) this.quality = 0;
 
     }
 
